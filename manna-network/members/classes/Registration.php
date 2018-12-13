36bb759c6c64531692619766896811aa239f70a8
Manna-Network <robert.r.lefebvre@gmail.com>
2018-12-13 14:27:45 -0500

    index.php

--- manna-network/members/classes/Registration.php
+++ manna-network/members/classes/Registration.php
@@ -440,15 +440,17 @@ header('Location:".$link."');
     {
 
 include(dirname(__DIR__, 3)."/manna-configs/db_cfg/agent_config.php");
-
+echo 'user id = ', $user_id;
 
         // if database connection opened
         if ($this->databaseConnection()) {
 
            // try to update user with specified information
             $query_update_user = $this->db_connection->prepare('UPDATE users SET user_active = 1, user_activation_hash = NULL WHERE user_id = :user_id AND user_activation_hash = :user_activation_hash');
+
             $query_update_user->bindValue(':user_id', intval(trim($user_id)), PDO::PARAM_INT);
             $query_update_user->bindValue(':user_activation_hash', $user_activation_hash, PDO::PARAM_STR);
+
             $query_update_user->execute();
 
             if ($query_update_user->rowCount() > 0) {
