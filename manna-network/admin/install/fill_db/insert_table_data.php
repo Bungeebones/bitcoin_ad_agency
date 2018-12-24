54445d89a45bab8e3ced78a9db8195c8c9171416
Robert Lefebvre <robert@bungeebones.com>
2018-12-24 14:53:06 -0500

    Update insert_table_data.php

--- manna-network/admin/install/fill_db/insert_table_data.php
+++ manna-network/admin/install/fill_db/insert_table_data.php
@@ -4,11 +4,6 @@ echo 'Now that we have the agent user downloaded the weekly zip and unpacking it
 <br>2) In the foreach section below, we need to get the name of the file and match it to the right table and insert<br>
 ';
 
-//test file permissions
-
-
-
-
 
 function dirname_safe($path, $level = 0){
     $dir = explode(DIRECTORY_SEPARATOR, $path);
@@ -17,17 +12,7 @@ function dirname_safe($path, $level = 0){
     array_splice($dir, $level);
     return implode($dir, DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
 }
-echo 'dirname_safe = ';
-print_r(dirname_safe(__DIR__, 4));
 
-$site_path = $_SERVER['DOCUMENT_ROOT'] ;
-  if($_SERVER['SERVER_PORT']=="443"){
-   $site_url="https://".$_SERVER['SERVER_NAME'] ;
-  }
-  else
-  {
-   $site_url="http://".$_SERVER['SERVER_NAME'] ;
-  }
 
 if( !defined( __DIR__ ) ) define( __DIR__, dirname(__FILE__) );
 
@@ -38,84 +23,57 @@ require_once( dirname_safe(__DIR__, 4).'manna-configs/db_cfg/auth_constants.php'
 require_once( dirname_safe(__DIR__, 4).'manna-configs/db_cfg/'.WRITER_AGENTS);
 require_once( dirname_safe(__DIR__, 4).'manna-configs/db_cfg/agent_config.php');
 	$mysqli = new mysqli($servername, $username, $password, $dbname);
-/*	$folder_name = "agents_sql_files";
-          $files = scandir($folder_name);
-	foreach ($files as &$value) {
-		if($value != "." && $value != ".."){
-		//build the sql insert query
-		$tobinserted = file_get_contents(__DIR__) .$folder_name.'/'.$value;
-echo '<br>install_folder = ', $_SERVER ['REQUEST_URI'];
-$request_uri = $_SERVER ['REQUEST_URI'];
-$pieces = explode("/",$request_uri);			
-$tobinserted = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/'.$pieces[1].'/admin/install/fill_db/manna_daily/'.$folder_name.'/'.$value);
-if ($mysqli->query($tobinserted) === TRUE) {
-			   echo "<h3>$value data inserted successfully</h3>";
-			} else {
-			    echo "<h3>Error inserting $value data: " . $mysqli->error ."</h3>";
-			}
-		}
-	    }
-*/
+
 $dir = dirname(__FILE__).DIRECTORY_SEPARATOR."manna_daily/";
-echo '$dir = ', $dir;
 
-  $yourStartingPath = "manna_daily";
-function listFolderFiles($dir)
-{
-    $fileInfo     = scandir($dir);
-    $allFileLists = [];
+	function listFolderFiles($dir)
+	{
+	    $fileInfo     = scandir($dir);
+	    $allFileLists = [];
 
-foreach ($fileInfo as $folder) {
-    if ($folder !== '.' && $folder !== '..') {
-        if (is_dir($dir . DIRECTORY_SEPARATOR . $folder)     === true) {
-            $allFileLists[$folder . '/'] = listFolderFiles($dir .     DIRECTORY_SEPARATOR . $folder);
-echo 'Folder = '. $folder. '  <br>';
-            } else {
-                echo' ';
-            }
-        }
-    }
-
-    return $allFileLists;
-}//end listFolderFiles()
-
-//listFolderFiles('manna_daily');
-//$dir = listFolderFiles('manna_daily');
-//echo '<pre>$dir = ';
-//print_r($dir);
-//echo '</pre>';
-
-//echo '<br>listFolderFiles = ', listFolderFiles('manna_daily');
+		foreach ($fileInfo as $folder) {
+		    if ($folder !== '.' && $folder !== '..') {
+			if (is_dir($dir . DIRECTORY_SEPARATOR . $folder)     === true) {
+			    $allFileLists[$folder . '/'] = listFolderFiles($dir .     DIRECTORY_SEPARATOR . $folder);
+			      echo 'Folder = '. $folder. '  <br>';
+			    } else {
+				echo' ';
+			    }
+			}
+		    }
+	    return $allFileLists;
+	}//end listFolderFiles()
 
 $files = scandir('manna_daily');
 foreach ($files as &$value) {
 
-if($value != "index.php" && $value != ".."&& $value != "."){
-$folder_name = $value;
-$files2 = scandir('manna_daily/'.$folder_name);
+	if($value != "index.php" && $value != ".."&& $value != "."){
+	$folder_name = $value;
+	$files2 = scandir('manna_daily/'.$folder_name);
 
-	foreach ($files2 as $value2) {
-if($value2 != "index.php" && $value2 != ".." && $value2 != "."){
+	   foreach ($files2 as $value2) {
+		if($value2 != "index.php" && $value2 != ".." && $value2 != "."){
 
-echo '<h1>opening file ', $_SERVER['DOCUMENT_ROOT'].'/'.AGENT_FOLDERNAME.'/admin/install/fill_db/manna_daily/'.$folder_name.'/'.$value2;
-echo '</h1>';
+		echo '<h1>opening file ', $_SERVER['DOCUMENT_ROOT'].'/'.AGENT_FOLDERNAME.'/admin/install/fill_db/manna_daily/'.$folder_name.'/'.$value2;
+		echo '</h1>';
 
-	$iniinserted = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/'.AGENT_FOLDERNAME.'/admin/install/fill_db/manna_daily/'.$folder_name.'/'.$value2);
+			$iniinserted = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/'.AGENT_FOLDERNAME.'/admin/install/fill_db/manna_daily/'.$folder_name.'/'.$value2);
 
-$tobinserted = explode(";", $iniinserted);
-foreach($tobinserted as$key=>$value3){
-echo '<br>Inserting ', $value3;
-	if ($mysqli->query($value3) === TRUE) {
-				   echo "<h3>$value2 data inserted successfully</h3>";
+		$tobinserted = explode(";", $iniinserted);
+			foreach($tobinserted as$key=>$value3){
+			echo '<br>Inserting ', $value3;
+				if ($mysqli->query($value3) === TRUE) {
+				   echo "<h3>$value2 data inserted successfully</h3><br>Still loading ... ";
 				} else {
-                                        if($mysqli->error !="Query was empty"){
+				  if($mysqli->error !="Query was empty"){
 				    echo "<h3>Error inserting $value2 data: " . $mysqli->error ."</h3>";
-                                    }
+				    }
 				}
-}
-}
+			}
+		}
+	    }
+echo '<br>Your web directory should be fully populated with links and categories,functioning and viewable at bitcoin_ad_agency/agent-dir/index.php.<br>ATTENTION! Visit the "Add URL" page (from the link in the upper nav bar of the directory) for the last configuration.'; 
 	}
-}
 
 }
 	
