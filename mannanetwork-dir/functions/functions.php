c0ac0fd9926bc75f4d827d3e6c52ad49b9f805b8
Robert Lefebvre <robert@bungeebones.com>
2019-02-19 13:40:24 -0500

    Update functions.php

--- mannanetwork-dir/functions/functions.php
+++ mannanetwork-dir/functions/functions.php
@@ -54,6 +54,81 @@ $id = 1;
 include(dirname( __FILE__, 3 ). "/manna-configs/db_cfg/".READER_AGENTS);
 include(dirname( __FILE__, 3 ). "/manna-configs/db_cfg/mysqli_connect.php");
 	$query = 'SELECT * FROM `categories` WHERE parent='.$id.' ORDER BY name;';
+	$result= mysqli_query($mysqli, $query);
+     if(mysqli_num_rows($result) > 0){  
+	while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+	$send_array[] = $row ;
+	}
+	return $send_array;
+}
+else
+{
+return "<h1>Sorry, No Sub-categories Found. </h1>"; 
+
+}
+}
+
+
+
+
+
+function categoryName($id){
+if (!defined('READER_AGENTS')) {
+include(dirname( __FILE__, 3 ). "/manna-network/db_cfg/auth_constants.php");
+}
+include(dirname( __FILE__, 3 ). "/manna-configs/db_cfg/".READER_AGENTS);
+include(dirname( __FILE__, 3 ). "/manna-configs/db_cfg/mysqli_connect.php");
+$id = mysqli_real_escape_string($mysqli, $id);
+	$query = 'SELECT name FROM `categories` WHERE id="'.$id.'"';
+
+	$result = mysqli_query($mysqli, $query);
+
+	$row = mysqli_fetch_row($result);
+
+	return $row[0];
+
+}
+
+function categoryPath($catid, $regional_number){
+if (!defined('READER_AGENTS')) {
+include(dirname( __FILE__, 3 ). "/manna-network/db_cfg/auth_constants.php");
+}
+include(dirname( __FILE__, 3 ). "/manna-configs/db_cfg/".READER_AGENTS);
+include(dirname( __FILE__, 3 ). "/manna-configs/db_cfg/mysqli_connect.php");
+	$path = '';
+$catid=mysqli_real_escape_string($mysqli, $catid);
+	$query = 'SELECT lft,rgt FROM `categories` WHERE id="'.$catid.'"';
+
+	$result = mysqli_query($mysqli, $query);
+	$row = mysqli_fetch_array($result);
+	$lft = $row['lft'];
+	$rgt = $row['rgt'];
+	$query = 'SELECT name,id FROM `categories` WHERE lft < '.$lft.' AND rgt > '.$rgt.' ORDER BY lft ASC';
+	$result = mysqli_query($mysqli, $query);
+	while($row = @mysqli_fetch_array($result)){
+		if($row['id'] != '1')
+if($regional_number != ""){
+$path .= ' | <a href="/'.$folder_name.'/'.$file_name.'/'. $affiliate_num.'/'.$row['id'].'///////'.$regional_number.'">'.$row['name'].'</a>';
+}
+else
+{
+$path .= ' | <a href="/'.$folder_name.'/'.$file_name.'/'. $affiliate_num.'/'.$row['id'].'">'.$row['name'].'</a>';
+}
+	}
+
+
+
+	return $path;
+}
+
+?>
+}
+if($id == ""){
+$id = 1;
+}
+include(dirname( __FILE__, 3 ). "/manna-configs/db_cfg/".READER_AGENTS);
+include(dirname( __FILE__, 3 ). "/manna-configs/db_cfg/mysqli_connect.php");
+	$query = 'SELECT * FROM `categories` WHERE parent='.$id.' ORDER BY name;';
 	$query= mysqli_query($mysqli, $query);
      if(mysqli_num_rows($query) > 0){  
 	while ($row = mysqli_fetch_array($query, MYSQLI_ASSOC)){
