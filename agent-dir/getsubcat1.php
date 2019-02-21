1d9da79332dc80c0c1db54a338fe9ea8e4ae23a0
Robert Lefebvre <robert@bungeebones.com>
2019-02-21 17:09:05 -0500

    Update getsubcat1.php

--- agent-dir/getsubcat1.php
+++ agent-dir/getsubcat1.php
@@ -1,4 +1,5 @@
 <?php
+
 $locus_array = "";
 $link_record_num = "";
 $link_page_total = ""; 
@@ -9,40 +10,23 @@ $cat_page_num = "";
 $category_id = ""; 
 $lnk_num = "";
 
-      include(dirname( __FILE__, 2 ).'/manna-configs/db_cfg/agent_config.php');
-
-$args = array();
-if(isset($_GET['regional_num'])){$args['regional_num']=  $_GET['regional_num'];}
-if(isset($link_record_num)){$args['link_record_num']=  $link_record_num;}
-if(isset($link_page_total)){$args['link_page_total']=  $link_page_total;} 
-if(isset($link_page_id)){$args['link_page_id']=  $link_page_id; }
-if(isset($pagem_url_cat)){$args['pagem_url_cat']=  $pagem_url_cat;}
-if(isset($link_page_num)){$args['link_page_num']=  $link_page_num;} 
-if(isset($cat_page_num)){$args['cat_page_num']=  $cat_page_num;} 
-if(isset($_GET['q'])){$args['category_id']=  $_GET['q']; }
-if(isset($lnk_num)){$args['lnk_num']=  $lnk_num;}
-$args['http_host']=   $_SERVER['HTTP_HOST'];
-
-$handle = curl_init();
-
-$url = CURL_SECURITY.AGENT_URL."/".AGENT_FOLDERNAME."/mannanetwork-dir/get_category_json.php";
-
-// Set the url
-curl_setopt($handle, CURLOPT_URL, $url);
-curl_setopt($handle, CURLOPT_POSTFIELDS,$args);
-// Set the result output to be a string.
-curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
- $jsonlinkList = curl_exec($handle);
- curl_close($handle);
-
-
+include(dirname( __FILE__, 2 ). "/manna-configs/db_cfg/agent_config.php");
+include(dirname( __FILE__, 2 )). "/mannanetwork-dir/functions/functions.php";
+$category_id  =  $_GET['q'];
+$comboList = getCategoryChildren($category_id);
 
 require_once('translations/en.php');
 
+$menu_str = '<form action="'. htmlspecialchars($_SERVER["PHP_SELF"], ENT_QUOTES, "utf-8").'"><select name="subCat'.$_GET['subcat'].'" onchange="updatecategoryButton(this.value), showSubCat';
+if($_GET['subcat'] == 4){
+$menu_str .= '4';
+}
+else
+{
+$menu_str .= $_GET['subcat']+1;
+}
 
-$comboList = json_decode($jsonlinkList, true);
-
-$menu_str = '<form action="'. htmlspecialchars($_SERVER["PHP_SELF"], ENT_QUOTES, "utf-8").'"><select name="subCat1" onchange="updatecategoryButton(this.value), showSubCat2(this.value)">
+$menu_str .= '(this.value)">
 <option value="">'.WORDING_AJAX_MENU1.'</option> ';
 foreach($comboList as $key=>$value){
  if($comboList[$key]['lft']+1 < $comboList[$key]['rgt']){
